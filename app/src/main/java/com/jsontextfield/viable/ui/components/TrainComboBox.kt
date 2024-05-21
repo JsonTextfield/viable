5b56a0c9a44a2a2f695cd48e844eaa7832668f58
jason <tehblackboy@gmail.com>
2024-05-20 21:05:28 -0400

    updated TrainComboBox

--- app/src/main/java/com/jsontextfield/viable/ui/components/TrainComboBox.kt
+++ app/src/main/java/com/jsontextfield/viable/ui/components/TrainComboBox.kt
@@ -1,5 +1,6 @@
-package com.jsontextfield.viable
+package com.jsontextfield.viable.ui.components
 
+import android.text.Html
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
@@ -7,7 +8,6 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.ListItem
-import androidx.compose.material3.ListItemDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -16,7 +16,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
+import com.jsontextfield.viable.R
 import com.jsontextfield.viable.entities.Train
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -36,16 +38,17 @@ fun TrainComboBox(
         ListItem(
             headlineContent = {
                 Text(
-                    selectedItem?.number ?: "",
+                    selectedItem?.toString() ?: "",
                     maxLines = 2,
                     overflow = TextOverflow.Ellipsis,
                 )
             },
             trailingContent = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
-            modifier = Modifier.fillMaxWidth().menuAnchor()
+            modifier = Modifier
+                .fillMaxWidth()
+                .menuAnchor()
         )
 
-        // Dropdown menu with list of items
         DropdownMenu(
             expanded = expanded,
             onDismissRequest = { expanded = false }
@@ -57,9 +60,28 @@ fun TrainComboBox(
                     text = {
                         ListItem(
                             modifier = Modifier.alpha(if (train.location != null) 1f else 0.5f),
-                            headlineContent = { Text(train.number) },
-                            supportingContent = { Text(train.status) },
-                            colors = ListItemDefaults.colors(),
+                            headlineContent = {
+                                Text(train.toString())
+                            },
+                            supportingContent = {
+                                Text(
+                                    if (!train.departed) {
+                                        stringResource(id = R.string.departed)
+                                    }
+                                    else if (train.arrived) {
+                                        stringResource(id = R.string.arrived)
+                                    }
+                                    else {
+                                        stringResource(
+                                            id = R.string.next_stop, train.nextStop?.name ?: "",
+                                            Html.fromHtml(
+                                                train.nextStop?.eta ?: "",
+                                                Html.FROM_HTML_MODE_LEGACY
+                                            ),
+                                        )
+                                    },
+                                )
+                            },
                         )
                     },
                     onClick = {
