233fd41fd34ee4daf72b0439eb16fd2289fb9d3e
jason <tehblackboy@gmail.com>
2024-05-20 21:04:44 -0400

    added view model to MainActivity and MainScreen

--- app/src/main/java/com/jsontextfield/viable/MainActivity.kt
+++ app/src/main/java/com/jsontextfield/viable/MainActivity.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import com.jsontextfield.viable.ui.ViableViewModel
 import com.jsontextfield.viable.ui.components.MainScreen
 import com.jsontextfield.viable.ui.theme.ViableTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val viableViewModel: ViableViewModel by viewModels { ViableViewModel.ViableViewModelFactory }
         enableEdgeToEdge()
-        setContent { ViableTheme { MainScreen() } }
+        setContent { ViableTheme { MainScreen(viableViewModel) } }
     }
 }
--- app/src/main/java/com/jsontextfield/viable/ui/components/MainScreen.kt
+++ app/src/main/java/com/jsontextfield/viable/ui/components/MainScreen.kt
@@ -1,7 +1,6 @@
 package com.jsontextfield.viable.ui.components
 
 import android.text.Html
-import android.util.Log
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
@@ -10,60 +9,59 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
-import com.android.volley.toolbox.JsonObjectRequest
-import com.android.volley.toolbox.Volley
 import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MapStyleOptions
+import com.google.android.gms.maps.model.RoundCap
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.MapProperties
 import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.Polyline
 import com.google.maps.android.compose.rememberCameraPositionState
 import com.jsontextfield.viable.R
-import com.jsontextfield.viable.entities.Train
+import com.jsontextfield.viable.Station
+import com.jsontextfield.viable.ui.ViableViewModel
 import kotlinx.coroutines.delay
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MainScreen() {
+fun MainScreen(viableViewModel: ViableViewModel) {
     val context = LocalContext.current
-    var selectedTrain by remember { mutableStateOf<Train?>(null) }
-    var trains by remember { mutableStateOf(ArrayList<Train>()) }
+    val viableState by viableViewModel.viableState.collectAsState()
+    var routeLine by remember { mutableStateOf(ArrayList<LatLng>()) }
+    var nextStation by remember { mutableStateOf<Station?>(null) }
     val cameraPositionState = rememberCameraPositionState()
+    val selectedTrain = viableState.selectedTrain
 
-    fun downloadData(callback: (List<Train>) -> Unit = {}) {
-        val url = "https://tsimobile.viarail.ca/data/allData.json"
-        val jsonObjectRequest = JsonObjectRequest(url, { response ->
-            val data = response.keys().asSequence().map {
-                response.getJSONObject(it).put("number", it)
-                Train.fromJson(response.getJSONObject(it))
-            }.sortedBy { train ->
-                train.number.split(" ").first().toInt()
-            }.toList()
-            callback(data)
-        }, {
-            callback(ArrayList())
-        })
-        Volley.newRequestQueue(context).add(jsonObjectRequest)
+    LaunchedEffect(true) {
+        while (true) {
+            viableViewModel.downloadData(context)
+            delay(30_000)
+        }
     }
-
-    fun callback(data: List<Train>) {
-        trains = ArrayList(data)
-        selectedTrain = trains.find { train ->
-            train.number == selectedTrain?.number
-        } ?: trains.firstOrNull()
-
-        Log.e("SelectedTrain", "$selectedTrain")
+    LaunchedEffect(selectedTrain) {
         selectedTrain?.let { train ->
             train.location?.let { latLon ->
                 cameraPositionState.move(CameraUpdateFactory.newLatLng(latLon.toLatLng()))
+            }
+            viableViewModel.getLine(train) { shapes ->
+                routeLine = ArrayList(shapes.map { shape ->
+                    LatLng(shape.lat, shape.lon)
+                })
+            }
+            viableViewModel.getNextStation(train) {
+                nextStation = it
             }
         }
     }
@@ -71,32 +69,20 @@ fun MainScreen() {
         topBar = {
             TopAppBar(
                 title = {
-                    TrainComboBox(items = trains, selectedItem = selectedTrain) {
-                        selectedTrain = it
-                        cameraPositionState.move(
-                            CameraUpdateFactory.newLatLng(
-                                selectedTrain!!.location!!.toLatLng()
-                            )
-                        )
+                    TrainComboBox(items = viableState.trains, selectedItem = selectedTrain) {
+                        viableViewModel.onTrainSelected(it)
                     }
                 },
             )
         },
     ) { innerPadding ->
-
-        LaunchedEffect(true) {
-            while (true) {
-                downloadData { callback(it) }
-                delay(30_000)
-            }
-        }
-
         Column(modifier = Modifier.padding(innerPadding)) {
             GoogleMap(
                 modifier = Modifier.weight(.5f),
                 cameraPositionState = cameraPositionState,
                 properties = MapProperties(
                     minZoomPreference = 5f,
+                    maxZoomPreference = 15f,
                     mapStyleOptions = if (isSystemInDarkTheme()) {
                         MapStyleOptions.loadRawResourceStyle(context, R.raw.dark_mode)
                     }
@@ -106,10 +92,17 @@ fun MainScreen() {
                 ),
             ) {
                 selectedTrain?.let { train ->
+                    Polyline(
+                        endCap = RoundCap(),
+                        startCap = RoundCap(),
+                        points = routeLine,
+                        color = colorResource(id = R.color.primary_colour),
+                    )
                     train.location?.let {
                         Marker(
                             state = MarkerState(position = it.toLatLng()),
-                            title = selectedTrain?.number,
+                            title = selectedTrain.toString(),
+                            icon = BitmapDescriptorFactory.fromAsset("train_24dp_FILL1_wght300_GRAD0_opsz24.png"),
                             snippet = if (!train.departed) {
                                 stringResource(id = R.string.departed)
                             }
@@ -127,6 +120,13 @@ fun MainScreen() {
                             },
                         )
                     }
+                }
+                nextStation?.let {
+                    Marker(
+                        state = MarkerState(position = LatLng(it.lat, it.lon)),
+                        title = it.name,
+                        icon = BitmapDescriptorFactory.fromAsset("things_to_do_24dp_FILL1_wght300_GRAD0_opsz24.png"),
+                    )
                 }
             }
             StopsList(stops = selectedTrain?.stops ?: emptyList(), modifier = Modifier.weight(.3f))
