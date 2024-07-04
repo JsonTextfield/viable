428c00a32c404b858726a8b01a5c77e95e857603
jason <tehblackboy@gmail.com>
2024-07-03 21:16:26 -0400

    added error handling and logging to Downloader

--- app/src/main/java/com/jsontextfield/viable/network/Downloader.kt
+++ app/src/main/java/com/jsontextfield/viable/network/Downloader.kt
@@ -1,27 +1,35 @@
 package com.jsontextfield.viable.network
 
+import android.util.Log
 import com.jsontextfield.viable.data.model.Train
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import okio.IOException
 import org.json.JSONObject
 
 object Downloader {
-    private val client = OkHttpClient()
+    private const val TAG = "Downloader"
+    private val client: OkHttpClient by lazy { OkHttpClient() }
 
-    fun downloadTrains() : List<Train> {
+    fun downloadTrains(): List<Train> {
         val url = "https://tsimobile.viarail.ca/data/allData.json"
         val request = Request.Builder().url(url).build()
-        val response = client.newCall(request).execute()
-        if (response.isSuccessful) {
-            val jsonObject = JSONObject(response.body?.string() ?: "")
+        try {
+            val response = client.newCall(request).execute()
+            if (response.isSuccessful) {
+                val jsonObject = JSONObject(response.body?.string() ?: "")
 
-            val data = jsonObject.keys().asSequence().map {
-                jsonObject.getJSONObject(it).put("number", it)
-                Train.fromJson(jsonObject.getJSONObject(it))
-            }.sortedBy { train ->
-                train.number.split(" ").first().toInt()
-            }.toList()
-            return data
+                val data = jsonObject.keys().asSequence().map {
+                    jsonObject.getJSONObject(it).put("number", it)
+                    Train.fromJson(jsonObject.getJSONObject(it))
+                }.sortedBy { train ->
+                    train.number.split(" ").first().toInt()
+                }.toList()
+                response.close()
+                return data
+            }
+        } catch (exception: IOException) {
+            Log.e(TAG, exception.stackTraceToString())
         }
         return emptyList()
     }
