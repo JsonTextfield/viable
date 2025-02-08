b60baf0f6487f98c65ad7639fd4d50e40ce8c2bf
jason <tehblackboy@gmail.com>
2025-02-08 12:03:22 -0500

    updated DateTimeFormatter for stop schedule time

--- app/src/main/java/com/jsontextfield/viable/data/model/Stop.kt
+++ app/src/main/java/com/jsontextfield/viable/data/model/Stop.kt
@@ -8,16 +8,19 @@ data class Stop(
     val eta: String = "",
     val id: String = "",
     val delay: Int = 0,
+    val scheduled: String = "",
 ) {
     companion object {
         fun fromJson(jsonObject: JSONObject): Stop {
-            val dateFormat = DateTimeFormatter.ISO_INSTANT
-            val scheduled = dateFormat.parse(jsonObject.optString("scheduled"))
+            val inFormatter = DateTimeFormatter.ISO_INSTANT
+            val outFormatter = DateTimeFormatter.ofPattern("HH:mm")
+            val schedule = outFormatter.format(inFormatter.parse(jsonObject.optString("scheduled")))
             return Stop(
                 name = jsonObject.optString("station"),
                 eta = jsonObject.optString("eta"),
                 id = jsonObject.optString("code"),
                 delay = jsonObject.optInt("diffMin"),
+                scheduled = schedule,
             )
         }
     }
