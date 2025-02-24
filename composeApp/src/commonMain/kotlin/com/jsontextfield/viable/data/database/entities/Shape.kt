4dfe606e474894f3e2ca2a351b135ff3dd989efb
jason <tehblackboy@gmail.com>
2025-02-24 00:18:25 -0500

    added serialization to entities

--- composeApp/src/commonMain/kotlin/com/jsontextfield/viable/data/database/entities/Shape.kt
+++ composeApp/src/commonMain/kotlin/com/jsontextfield/viable/data/database/entities/Shape.kt
@@ -2,12 +2,15 @@ package com.jsontextfield.viable.data.database.entities
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 
+@Serializable
 @Entity(tableName = "shapes", primaryKeys = ["shape_id", "shape_pt_sequence"])
 data class Shape(
-    @ColumnInfo(name = "shape_id") val id: String,
-    @ColumnInfo(name = "shape_pt_lat") val lat: Double,
-    @ColumnInfo(name = "shape_pt_lon") val lon: Double,
-    @ColumnInfo(name = "shape_pt_sequence") val sequence: Int,
-    @ColumnInfo(name = "shape_dist_traveled") val distance: Double,
+    @SerialName("shape_id") @ColumnInfo(name = "shape_id") val id: String,
+    @SerialName("shape_pt_lat") @ColumnInfo(name = "shape_pt_lat") val lat: Double,
+    @SerialName("shape_pt_lon") @ColumnInfo(name = "shape_pt_lon") val lon: Double,
+    @SerialName("shape_pt_sequence") @ColumnInfo(name = "shape_pt_sequence") val sequence: Int,
+    @SerialName("shape_dist_traveled") @ColumnInfo(name = "shape_dist_traveled") val distance: Double? = null,
 )
--- composeApp/src/commonMain/kotlin/com/jsontextfield/viable/data/database/entities/Station.kt
+++ composeApp/src/commonMain/kotlin/com/jsontextfield/viable/data/database/entities/Station.kt
@@ -3,16 +3,46 @@ package com.jsontextfield.viable.data.database.entities
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 
+@Serializable
 @Entity(tableName = "stops")
 data class Station(
-    @PrimaryKey @ColumnInfo(name = "stop_id") val id: String,
-    @ColumnInfo(name = "stop_code") val code: String,
-    @ColumnInfo(name = "stop_name") val name: String,
-    @ColumnInfo(name = "location_type") val locationType: Int,
-    @ColumnInfo(name = "stop_lat") val lat: Double,
-    @ColumnInfo(name = "stop_lon") val lon: Double,
-    @ColumnInfo(name = "stop_timezone") val timezone: String,
-    @ColumnInfo(name = "parent_station") val parentStation: String,
-    @ColumnInfo(name = "wheelchair_boarding") val wheelchairBoarding: Int,
+    @PrimaryKey
+    @SerialName("stop_id")
+    @ColumnInfo(name = "stop_id")
+    val id: String,
+
+    @SerialName("stop_code")
+    @ColumnInfo(name = "stop_code")
+    val code: String,
+
+    @SerialName("stop_name")
+    @ColumnInfo(name = "stop_name")
+    val name: String,
+
+    @SerialName("location_type")
+    @ColumnInfo(name = "location_type")
+    val locationType: Int,
+
+    @SerialName("stop_lat")
+    @ColumnInfo(name = "stop_lat")
+    val lat: Double,
+
+    @SerialName("stop_lon")
+    @ColumnInfo(name = "stop_lon")
+    val lon: Double,
+
+    @SerialName("stop_timezone")
+    @ColumnInfo(name = "stop_timezone")
+    val timezone: String,
+
+    @SerialName("parent_station")
+    @ColumnInfo(name = "parent_station")
+    val parentStation: String,
+
+    @SerialName("wheelchair_boarding")
+    @ColumnInfo(name = "wheelchair_boarding")
+    val wheelchairBoarding: Int? = null,
 )
