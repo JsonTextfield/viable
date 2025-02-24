9e73caa01543fd24346bfcea4b8cdabd7fda8d3d
jason <tehblackboy@gmail.com>
2025-02-24 00:19:43 -0500

    updated supabase query

--- composeApp/src/commonMain/kotlin/com/jsontextfield/viable/data/datasource/ViaRailRemoteDataSource.kt
+++ composeApp/src/commonMain/kotlin/com/jsontextfield/viable/data/datasource/ViaRailRemoteDataSource.kt
@@ -5,17 +5,20 @@ import com.jsontextfield.viable.data.database.entities.Station
 import io.github.jan.supabase.SupabaseClient
 import io.github.jan.supabase.postgrest.from
 import io.github.jan.supabase.postgrest.query.Columns
+import io.github.jan.supabase.postgrest.query.Order
 
 class ViaRailRemoteDataSource(
     private val supabaseClient: SupabaseClient
 ) : IViaRailDataSource {
     override suspend fun getRouteShape(route: String): List<Shape> {
-        return supabaseClient.from("trips").select {
-            select(Columns.raw("shapes.shape_id"))
-            filter {
-                eq("route_id", route)
-            }
-        }.decodeList<Shape>()
+        val result = supabaseClient.from("trips")
+            .select(Columns.raw("shapes(*)")) {
+                filter {
+                    eq("trip_short_name", route)
+                }
+                order("shape_pt_sequence", Order.ASCENDING, referencedTable = "shapes")
+            }.decodeSingle<Map<String, List<Shape>>>()
+        return result["shapes"] ?: emptyList()
     }
 
     override suspend fun getStation(code: String): Station {
--- composeApp/src/commonMain/kotlin/com/jsontextfield/viable/di/Koin.kt
+++ composeApp/src/commonMain/kotlin/com/jsontextfield/viable/di/Koin.kt
@@ -2,23 +2,41 @@ package com.jsontextfield.viable.di
 
 import androidx.sqlite.driver.bundled.BundledSQLiteDriver
 import com.jsontextfield.viable.data.database.DatabaseFactory
-import com.jsontextfield.viable.data.database.ViaRailDatabase
+import com.jsontextfield.viable.data.datasource.IViaRailDataSource
+import com.jsontextfield.viable.data.datasource.ViaRailRemoteDataSource
 import com.jsontextfield.viable.data.repositories.ITrainRepository
 import com.jsontextfield.viable.data.repositories.TrainRepository
 import com.jsontextfield.viable.network.TrainService
 import com.jsontextfield.viable.ui.ViableViewModel
+import io.github.jan.supabase.SupabaseClient
+import io.github.jan.supabase.createSupabaseClient
+import io.github.jan.supabase.postgrest.Postgrest
 import io.ktor.client.HttpClient
+import kotlinx.coroutines.runBlocking
+import org.jetbrains.compose.resources.getString
 import org.koin.core.context.startKoin
 import org.koin.core.module.Module
 import org.koin.core.module.dsl.factoryOf
 import org.koin.dsl.KoinAppDeclaration
 import org.koin.dsl.module
+import viable.composeapp.generated.resources.Res
+import viable.composeapp.generated.resources.supabase_key
 
 expect val platformModule: Module
 
 val networkModule = module {
     single<HttpClient> { HttpClient() }
     single<TrainService> { TrainService(get<HttpClient>()) }
+    single<SupabaseClient> {
+        runBlocking {
+            createSupabaseClient(
+                supabaseUrl = "https://mqfvppcezfwunrgqgucv.supabase.co",
+                supabaseKey = getString(Res.string.supabase_key),
+            ) {
+                install(Postgrest)
+            }
+        }
+    }
 }
 
 val dataModule = module {
@@ -27,9 +45,13 @@ val dataModule = module {
             .setDriver(BundledSQLiteDriver())
             .build()
     }
+    single<IViaRailDataSource> {
+        //ViaRailLocalDataSource(get<ViaRailDatabase>())
+        ViaRailRemoteDataSource(get<SupabaseClient>())
+    }
     single<ITrainRepository> {
         TrainRepository(
-            get<ViaRailDatabase>(),
+            get<IViaRailDataSource>(),
             get<TrainService>(),
         )
     }
