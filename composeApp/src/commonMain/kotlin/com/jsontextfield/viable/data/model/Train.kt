92455d0a75a8f8854cd25701362ecf7106bfc5a4
jason <tehblackboy@gmail.com>
2025-02-24 00:19:03 -0500

    moved json serialization to TrainService

--- composeApp/src/commonMain/kotlin/com/jsontextfield/viable/data/model/Train.kt
+++ composeApp/src/commonMain/kotlin/com/jsontextfield/viable/data/model/Train.kt
@@ -2,12 +2,6 @@ package com.jsontextfield.viable.data.model
 
 import androidx.compose.ui.text.intl.Locale
 import androidx.compose.ui.text.toLowerCase
-import kotlinx.serialization.json.JsonObject
-import kotlinx.serialization.json.booleanOrNull
-import kotlinx.serialization.json.doubleOrNull
-import kotlinx.serialization.json.jsonArray
-import kotlinx.serialization.json.jsonObject
-import kotlinx.serialization.json.jsonPrimitive
 
 data class Train(
     val number: String = "",
@@ -21,34 +15,6 @@ data class Train(
 
     val nextStop: Stop?
         get() = stops.find { it.eta != "ARR" }
-
-    companion object {
-        fun fromJson(key: String, jsonObject: JsonObject): Train {
-            val latLon = if (jsonObject["lat"] != null && jsonObject["lng"] != null) {
-                LatLon(
-                    jsonObject["lat"]?.jsonPrimitive?.doubleOrNull ?: 0.0,
-                    jsonObject["lng"]?.jsonPrimitive?.doubleOrNull ?: 0.0,
-                )
-            } else {
-                null
-            }
-            val stopsJson = jsonObject.get("times")?.jsonArray
-            val stops = ArrayList<Stop>()
-            for (i in 0 until (stopsJson?.size ?: 0)) {
-                stopsJson?.get(i)?.jsonObject?.let(Stop::fromJson)?.let(stops::add)
-            }
-            val headsign =
-                "${jsonObject["from"]?.jsonPrimitive?.content} -> ${jsonObject["to"]?.jsonPrimitive?.content}".toMixedCase()
-            return Train(
-                number = key,
-                headsign = headsign,
-                departed = jsonObject["departed"]?.jsonPrimitive?.booleanOrNull ?: false,
-                arrived = jsonObject["arrived"]?.jsonPrimitive?.booleanOrNull ?: false,
-                location = latLon,
-                stops = stops,
-            )
-        }
-    }
 }
 
 fun String.toMixedCase(): String {
--- composeApp/src/commonMain/kotlin/com/jsontextfield/viable/network/TrainService.kt
+++ composeApp/src/commonMain/kotlin/com/jsontextfield/viable/network/TrainService.kt
@@ -1,13 +1,20 @@
 package com.jsontextfield.viable.network
 
+import com.jsontextfield.viable.data.model.LatLon
+import com.jsontextfield.viable.data.model.Stop
 import com.jsontextfield.viable.data.model.Train
+import com.jsontextfield.viable.data.model.toMixedCase
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.get
 import io.ktor.http.HttpStatusCode
 import kotlinx.io.IOException
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.booleanOrNull
+import kotlinx.serialization.json.doubleOrNull
+import kotlinx.serialization.json.jsonArray
 import kotlinx.serialization.json.jsonObject
+import kotlinx.serialization.json.jsonPrimitive
 
 class TrainService(private val client: HttpClient) {
     suspend fun getTrains(): List<Train> {
@@ -15,15 +22,37 @@ class TrainService(private val client: HttpClient) {
         try {
             val response = client.get(url)
             if (response.status == HttpStatusCode.OK) {
-                val jsonObject =
-                    Json.parseToJsonElement(response.body<String>()).jsonObject
-                val data = jsonObject.keys.map {
-                    Train.fromJson(it, jsonObject[it]!!.jsonObject)
+                val data = Json.parseToJsonElement(response.body<String>()).jsonObject.entries
+                return data.map { entry ->
+                    val jsonObject = entry.value.jsonObject
+                    val latLon = if (jsonObject["lat"] != null && jsonObject["lng"] != null) {
+                        LatLon(
+                            jsonObject["lat"]?.jsonPrimitive?.doubleOrNull ?: 0.0,
+                            jsonObject["lng"]?.jsonPrimitive?.doubleOrNull ?: 0.0,
+                        )
+                    }
+                    else {
+                        null
+                    }
+                    val stopsJson = jsonObject["times"]?.jsonArray
+                    val stops = ArrayList<Stop>()
+                    for (i in 0 until (stopsJson?.size ?: 0)) {
+                        stopsJson?.get(i)?.jsonObject?.let(Stop::fromJson)?.let(stops::add)
+                    }
+                    val headsign =
+                        "${jsonObject["from"]?.jsonPrimitive?.content} -> ${jsonObject["to"]?.jsonPrimitive?.content}".toMixedCase()
+                    Train(
+                        number = entry.key,
+                        headsign = headsign,
+                        departed = jsonObject["departed"]?.jsonPrimitive?.booleanOrNull
+                            ?: false,
+                        arrived = jsonObject["arrived"]?.jsonPrimitive?.booleanOrNull ?: false,
+                        location = latLon,
+                        stops = stops,
+                    )
                 }.sortedBy { train ->
                     train.number.split(" ").first().toInt()
                 }.toList()
-
-                return data
             }
         } catch (exception: IOException) {
             //Log.e("Downloader", exception.stackTraceToString())
